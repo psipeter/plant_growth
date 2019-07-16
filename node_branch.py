46a462985bb36d2b140f5d03412aa5f77569b80b
Peter Duggins <psipeter@gmail.com>
2019-07-16 00:20:25 -0400

    recursive_diffusion partly working, but printing cell.x is off, and method is slow, and nodes still appear closeby

--- node_branch.py
+++ node_branch.py
@@ -14,28 +14,16 @@ import matplotlib.cm as cm
 import copy
 
 class Cell():
-	def __init__(self, id, x=0, y=0, theta=0):
-		self.id = id
+	def __init__(self, ID, x=0, y=0, theta=0):
+		self.ID = ID
 		self.x = x
 		self.y = y
 		self.theta = theta
-		self.to_node = to_node  # todo: 2-way communication to ensure even spacing
+		self.d_node = np.inf
 		self.cell_type = 'vein'
 		self.parent = []
 		self.children = []
-		self.up = None
-		self.down = None
-		self.left = None
-		self.right = None
-		self.rng = np.random.RandomState(seed=id)
-
-	def update_to_node(self, val):
-		self.to_node = np.max([0, val])
-
-	def update_cell_type(self):
-		if self.to_node == 0:
-			self.cell_type = 'node'
-			self.to_node = to_node
+		self.rng = np.random.RandomState(seed=ID)
 
 def pdf(rng):
 	# rule = {str(0): 1}
@@ -62,75 +50,74 @@ def recursive_push(pushed, vx, vy):
 		recursive_push(cell, vx, vy)
 	grid[x_new][y_new].append(pushed)
 
+def recursive_diffusion(cell):
+	if cell.cell_type == 'node':
+		return 0
+	d_nodes = [cell.d_node]
+	for parent in cell.parent:
+		d_nodes.append(recursive_diffusion(parent))
+	cell.d_node = np.min(d_nodes)
+	return int(1+cell.d_node)
+	
 
-# def recursive_push(pusher, pushed, children, vx, vy):
-# 	x_old = pushed.x
-# 	y_old = pushed.y
-# 	pushed.x += vx
-# 	pushed.y += vy
-# 	grid[x_old][y_old].remove(pushed)
-# 	x_new = pushed.x
-# 	y_new = pushed.y
+'''main'''
 
-# 	for cell in grid[x_new][y_new]:
-# 		children.append(cell)
-# 		new_children = recursive_push(pushed, cell, copy.deepcopy(cell.children), vx, vy)
-# 	grid[x_new][y_new].append(pushed)
-# 	pushed.parent.append(pusher)
-
-# 	for cell in grid[x_new][y_new]:
-# 		pushed.children.append(cell)
-# 		if cell in pusher.children:
-# 			pusher.children.remove(cell)
-# 	# print(pushed.children)
-# 	for cell in pushed.children:
-# 		recursive_push(pushed, cell, vx, vy)
-# 	grid[x_new][y_new].append(pushed)
-# 	pushed.parent.append(pusher)
-# 	pusher.children.append(pushed)
-
-'''main loop'''
-t_final = 7
+t_final = 6
 xmax = 100
 ymax = 100
 seed = 0
 p_child = 1.0
-to_node = 3
+node_spacing = 4
 rng = np.random.RandomState(seed=seed)
+# initialization
 grid0 = [[[] for x in range(xmax)] for y in range(ymax)]
-cell0 = Cell(id=0, x=1, y=int(ymax/2))
+node0 = Cell(ID=0, x=0, y=int(ymax/2))  # inactive node (not in cells)
+node0.cell_type = 'node'
+node0.d_node = 0
+cell0 = Cell(ID=1, x=1, y=int(ymax/2))
+cell0.parent.append(node0)
+cell0.d_node = 1
+# node0.children.append(cell0)
 grid0[int(cell0.x)][int(cell0.y)].append(cell0)
 cells = [cell0]
-grid_history = [copy.deepcopy(grid0)]
+nodes = [node0]
 cell_history = [copy.deepcopy(cells)]
 grid = grid0
-idmax = 0
+IDmax = 1
+# loop
 for t in range(t_final):
 	print('t=%s'%(t+1))
-	children = []
+	cells_new = []
 	for cell in cells:
+		# find distance to nearest node, maybe become a node
+		cell.d_node = recursive_diffusion(cell)
+		print(cell.x, cell.y, cell.d_node)
+		if cell.d_node == node_spacing:
+			cell.cell_type = 'node'
+			cell.d_node = 0
+		# reproduce
 		if rng.uniform(0, 1) < p_child:
-			idmax += 1
-			child = Cell(id=idmax, x=cell.x, y=cell.y, theta=cell.theta+pdf(cell.rng)*(cell.cell_type == 'node'))
-			child.update_to_node(cell.to_node-1)
-			child.update_cell_type()
-			children.append(child)
+			IDmax += 1
+			child = Cell(ID=IDmax, x=cell.x, y=cell.y, theta=cell.theta+pdf(cell.rng)*(cell.cell_type == 'node'))
+			if child.cell_type == 'node':
+				nodes.append(child)
+			cells_new.append(child)
 			cell.children.append(child)
+			child.parent.append(cell)
 			vx, vy = int(np.cos(child.theta)), int(np.sin(child.theta))  # todo: non-right angles
 			child.x += vx
 			child.y += vy
+			# push cells in offspring spawn point away
 			for pushed in grid[child.x][child.y]:
-				cell.children.remove(pushed)  # try block?
+				cell.children.remove(pushed)
 				child.children.append(pushed)
 				pushed.parent.append(child)
 				recursive_push(pushed, vx, vy)
 			grid[child.x][child.y].append(child)
-			# recursive_push(cell, child, vx, vy)
-	for child in children:
-		cells.append(child)
-	# rng.shuffle(cells)
+	for new in cells_new:
+		cells.append(new)
+	rng.shuffle(cells)
 	cell_history.append(copy.deepcopy(cells))
-	grid_history.append(copy.deepcopy(grid))
 	print('n_cells=%s' %len(cells))
 	print('nonzero grid', np.count_nonzero(np.array(grid)))
 	# print('where nonzero', np.nonzero(np.array(grid)))
@@ -142,6 +129,6 @@ for t, grid in enumerate(cell_history):
 	xs = np.array([cell.x for cell in grid])
 	ys = np.array([cell.y for cell in grid])
 	ss = np.array([10 if cell.cell_type == 'vein' else 40 for cell in grid])
-	cs = np.array([cell.id/len(cell_history[-1]) for cell in grid])
+	cs = np.array([cell.ID/len(cell_history[-1]) for cell in grid])
 	ax.scatter(xs, ys, s=ss, color=cm.rainbow(cs))
 	plt.savefig('plots/%s.png'%t)
