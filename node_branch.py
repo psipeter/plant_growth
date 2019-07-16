7554acd6c8c3125412ea25b615d49ffa006f5b2d
Peter Duggins <psipeter@gmail.com>
2019-07-16 13:40:26 -0400

    removed grid, improved plotting, extend recursion depth

--- node_branch.py
+++ node_branch.py
@@ -6,12 +6,10 @@ Final Project
 '''
 
 import numpy as np
-# import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-# from matplotlib import animation
-# matplotlib.use("Agg")
 import copy
+import sys
 
 class Cell():
 	def __init__(self, ID, x=0, y=0, theta=0):
@@ -20,6 +18,7 @@ class Cell():
 		self.y = y
 		self.theta = theta
 		self.d_node = np.inf
+		self.p_reproduce = p_reproduce_vein
 		self.cell_type = 'vein'
 		self.parent = None
 		self.children = []
@@ -37,13 +36,14 @@ class Cell():
 	def update_cell_type(self):
 		if self.d_node == node_spacing:
 			self.cell_type = 'node'
-			self.d_node = 0		
+			self.d_node = 0	
+			self.p_reproduce = p_reproduce_node	
 		# print(self.d_node == node_spacing)
 
 def pdf(rng):
 	# rule = {str(0): 1}
 	rule = {str(-np.pi/2): 0.5, str(np.pi/2): 0.5}
-	# rule = {str(-np.pi/2): 0.05, str(0): 0.9, str(np.pi/2): 0.05}
+	# rule = {str(-np.pi/2): 0.25, str(0): 0.5, str(np.pi/2): 0.25}
 	sample = rng.uniform(0, 1)
 	total = 0
 	for key, value in rule.items():
@@ -58,49 +58,44 @@ def recursive_push(pushed, vx, vy):
 	y_old = pushed.y
 	pushed.x += vx
 	pushed.y += vy
-	grid[x_old][y_old].remove(pushed)
 	x_new = pushed.x
 	y_new = pushed.y
 	for cell in pushed.children:
 		recursive_push(cell, vx, vy)
 	pushed.update_d_node()
 	pushed.update_cell_type()
-	grid[x_new][y_new].append(pushed)
 
 
 '''main'''
-
-t_final = 6
-xmax = 100
-ymax = 100
+t_final = 20
 seed = 0
-p_child = 1.0
+p_reproduce_vein = 0.5
+p_reproduce_node = 1.0
 node_spacing = 4
 rng = np.random.RandomState(seed=seed)
-# initialization
-grid0 = [[[] for x in range(xmax)] for y in range(ymax)]
-node0 = Cell(ID=0, x=0, y=int(ymax/2))  # inactive node (not in cells)
+node0 = Cell(ID=0, x=0, y=0)  # inactive node (not in cells)
 node0.cell_type = 'node'
 node0.d_node = 0
-cell0 = Cell(ID=1, x=1, y=int(ymax/2))
+cell0 = Cell(ID=1, x=1, y=0)
 cell0.parent = node0
 cell0.d_node = 1
-# node0.children.append(cell0)
-grid0[int(cell0.x)][int(cell0.y)].append(cell0)
 cells = [cell0]
-cell_history = [copy.deepcopy(cells)]
-grid = grid0
+xs = [[] for t in range(t_final)]
+ys = [[] for t in range(t_final)]
+cell_types = [[] for t in range(t_final)]
+IDs = [[] for t in range(t_final)]
 IDmax = 1
-# loop
+
 for t in range(t_final):
 	print('t=%s'%(t+1))
+	sys.setrecursionlimit(np.max([1000, 2*len(cells)]))
 	cells_new = []
 	for cell in cells:
 		# find distance to nearest node, maybe become a node
 		cell.update_d_node()
 		cell.update_cell_type()
 		# reproduce
-		if rng.uniform(0, 1) < p_child:
+		if rng.uniform(0, 1) < cell.p_reproduce:
 			IDmax += 1
 			child = Cell(ID=IDmax, x=cell.x, y=cell.y, theta=cell.theta+pdf(cell.rng)*(cell.cell_type == 'node'))
 			cells_new.append(child)
@@ -110,11 +105,14 @@ for t in range(t_final):
 			child.x += vx
 			child.y += vy
 			# push cells in offspring spawn point away
-			for pushed in grid[child.x][child.y]:
-				cell.children.remove(pushed)
-				child.children.append(pushed)
-				pushed.parent = child
-				recursive_push(pushed, vx, vy)
+			for pushed in cell.children:
+				if pushed is child:
+					continue
+				if pushed.x == child.x and pushed.y == child.y:
+					cell.children.remove(pushed)
+					child.children.append(pushed)
+					pushed.parent = child
+					recursive_push(pushed, vx, vy)					
 			# update child.d_node
 			child.update_d_node()
 			child.update_cell_type()
@@ -122,26 +120,45 @@ for t in range(t_final):
 			cell.update_cell_type()
 			child.update_d_node()
 			child.update_cell_type()
-			grid[child.x][child.y].append(child)
-			# print(cell.ID, 'births', child.ID)
-			# print(cell.ID, '(', cell.x, cell.y, ')', cell.d_node, cell.cell_type)
-			# print(child.ID, '(', child.x, child.y, ')', child.d_node, child.cell_type)
 	for new in cells_new:
 		cells.append(new)
 	rng.shuffle(cells)
-	cell_history.append(copy.deepcopy(cells))
+	for cell in cells:
+		xs[t].append(cell.x)
+		ys[t].append(cell.y)
+		cell_types[t].append(cell.cell_type)
+		IDs[t].append(cell.ID)
 	print('n_cells=%s' %len(cells))
-	print('nonzero grid', np.count_nonzero(np.array(grid)))
-	# print('where nonzero', np.nonzero(np.array(grid)))
 
-for t, grid in enumerate(cell_history):
+# to pass list of markers as an argument to scatter
+# https://github.com/matplotlib/matplotlib/issues/11155
+def mscatter(x,y,ax=None, m=None, **kw):
+    import matplotlib.markers as mmarkers
+    if not ax: ax=plt.gca()
+    sc = ax.scatter(x,y,**kw)
+    if (m is not None) and (len(m)==len(x)):
+        paths = []
+        for marker in m:
+            if isinstance(marker, mmarkers.MarkerStyle):
+                marker_obj = marker
+            else:
+                marker_obj = mmarkers.MarkerStyle(marker)
+            path = marker_obj.get_path().transformed(
+                        marker_obj.get_transform())
+            paths.append(path)
+        sc.set_paths(paths)
+    return sc
+
+for t in range(t_final):
+	gridsize = 1+np.max([np.max(xs[t]), np.max(ys[t])])
+	# sizes = np.array([np.sqrt(gridsize) if ct == 'vein' else 5*np.sqrt(gridsize) for ct in cell_types[t]])
+	shapes = np.array(["o" if ct == 'vein' else "D" for ct in cell_types[t]])
+	colors = np.array(IDs[t])/IDmax
+	colors = np.array(['k' if ct == 'vein' else 'r' for ct in cell_types[t]])
 	fig, ax = plt.subplots(figsize=((16, 16)))
-	ax.set(xlim=((0, xmax)), ylim=((0, ymax)), title='t=%s'%t)
+	ax.set(xlim=((-1, gridsize)), ylim=((-gridsize/2, gridsize/2)), title='t=%s'%t)
 	ax.axis('off')
-	xs = np.array([cell.x for cell in grid])
-	ys = np.array([cell.y for cell in grid])
-	ss = np.array([10 if cell.cell_type == 'vein' else 40 for cell in grid])
-	cs = np.array([cell.ID/len(cell_history[-1]) for cell in grid])
-	ax.scatter(xs, ys, s=ss, color=cm.rainbow(cs))
-	ax.scatter(node0.x, node0.y, s=40, color='b')
+	mscatter(xs[t], ys[t], m=shapes, c=colors)  # color=cm.rainbow(colors), ax=ax
+	# ax.scatter(xs[t], ys[t], s=sizes, marker=shapes, color=cm.rainbow(colors))
+	ax.scatter(node0.x, node0.y, c='r', marker="D")
 	plt.savefig('plots/%s.png'%t)
