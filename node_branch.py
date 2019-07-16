cd2d4d8ac64be0b6d0ed16e6a6ef13deef63b9d7
Peter Duggins <psipeter@gmail.com>
2019-07-16 11:57:09 -0400

    updating d_node and cell_type correctly spaces nodes

--- node_branch.py
+++ node_branch.py
@@ -21,9 +21,24 @@ class Cell():
 		self.theta = theta
 		self.d_node = np.inf
 		self.cell_type = 'vein'
-		self.parent = []
+		self.parent = None
 		self.children = []
 		self.rng = np.random.RandomState(seed=ID)
+
+	def update_d_node(self):
+		if self.cell_type == 'node':
+			self.d_node = 0
+			return
+		d_node_min = np.min([self.d_node, self.parent.d_node])
+		for child in self.children:
+			d_node_min = np.min([d_node_min, child.d_node])
+		self.d_node = int(d_node_min + 1)
+
+	def update_cell_type(self):
+		if self.d_node == node_spacing:
+			self.cell_type = 'node'
+			self.d_node = 0		
+		# print(self.d_node == node_spacing)
 
 def pdf(rng):
 	# rule = {str(0): 1}
@@ -48,17 +63,10 @@ def recursive_push(pushed, vx, vy):
 	y_new = pushed.y
 	for cell in pushed.children:
 		recursive_push(cell, vx, vy)
+	pushed.update_d_node()
+	pushed.update_cell_type()
 	grid[x_new][y_new].append(pushed)
 
-def recursive_diffusion(cell):
-	if cell.cell_type == 'node':
-		return 0
-	d_nodes = [cell.d_node]
-	for parent in cell.parent:
-		d_nodes.append(recursive_diffusion(parent))
-	cell.d_node = np.min(d_nodes)
-	return int(1+cell.d_node)
-	
 
 '''main'''
 
@@ -75,12 +83,11 @@ node0 = Cell(ID=0, x=0, y=int(ymax/2))  # inactive node (not in cells)
 node0.cell_type = 'node'
 node0.d_node = 0
 cell0 = Cell(ID=1, x=1, y=int(ymax/2))
-cell0.parent.append(node0)
+cell0.parent = node0
 cell0.d_node = 1
 # node0.children.append(cell0)
 grid0[int(cell0.x)][int(cell0.y)].append(cell0)
 cells = [cell0]
-nodes = [node0]
 cell_history = [copy.deepcopy(cells)]
 grid = grid0
 IDmax = 1
@@ -90,20 +97,15 @@ for t in range(t_final):
 	cells_new = []
 	for cell in cells:
 		# find distance to nearest node, maybe become a node
-		cell.d_node = recursive_diffusion(cell)
-		print(cell.x, cell.y, cell.d_node)
-		if cell.d_node == node_spacing:
-			cell.cell_type = 'node'
-			cell.d_node = 0
+		cell.update_d_node()
+		cell.update_cell_type()
 		# reproduce
 		if rng.uniform(0, 1) < p_child:
 			IDmax += 1
 			child = Cell(ID=IDmax, x=cell.x, y=cell.y, theta=cell.theta+pdf(cell.rng)*(cell.cell_type == 'node'))
-			if child.cell_type == 'node':
-				nodes.append(child)
 			cells_new.append(child)
 			cell.children.append(child)
-			child.parent.append(cell)
+			child.parent = cell
 			vx, vy = int(np.cos(child.theta)), int(np.sin(child.theta))  # todo: non-right angles
 			child.x += vx
 			child.y += vy
@@ -111,9 +113,19 @@ for t in range(t_final):
 			for pushed in grid[child.x][child.y]:
 				cell.children.remove(pushed)
 				child.children.append(pushed)
-				pushed.parent.append(child)
+				pushed.parent = child
 				recursive_push(pushed, vx, vy)
+			# update child.d_node
+			child.update_d_node()
+			child.update_cell_type()
+			cell.update_d_node()
+			cell.update_cell_type()
+			child.update_d_node()
+			child.update_cell_type()
 			grid[child.x][child.y].append(child)
+			# print(cell.ID, 'births', child.ID)
+			# print(cell.ID, '(', cell.x, cell.y, ')', cell.d_node, cell.cell_type)
+			# print(child.ID, '(', child.x, child.y, ')', child.d_node, child.cell_type)
 	for new in cells_new:
 		cells.append(new)
 	rng.shuffle(cells)
@@ -131,4 +143,5 @@ for t, grid in enumerate(cell_history):
 	ss = np.array([10 if cell.cell_type == 'vein' else 40 for cell in grid])
 	cs = np.array([cell.ID/len(cell_history[-1]) for cell in grid])
 	ax.scatter(xs, ys, s=ss, color=cm.rainbow(cs))
+	ax.scatter(node0.x, node0.y, s=40, color='b')
 	plt.savefig('plots/%s.png'%t)
